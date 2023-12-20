4b4e1ba3d136f9ccb1ad7013bee193ce2e59e23d
pr-citrate <pr.citrate@gmail.com>
2023-12-20 10:23:49 +0900

    removed auth and db

--- src/app/userposts/page.jsx
+++
@@ -1,7 +0,0 @@
-export default function UserPosts() {
-  return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      UserPosts
-    </main>
-  );
-}
--- src/providers/ServerSessionProvider.jsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { SessionProvider } from "next-auth/react";
-
-export default function ServerSessionProvider({ children }) {
-  return <SessionProvider>{children}</SessionProvider>;
-}
