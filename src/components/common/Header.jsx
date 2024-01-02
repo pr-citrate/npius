6179ca06ba15422e6bb240c08f1ee2fbda6dd0d7
pr-citrate <pr.citrate@gmail.com>
2024-01-03 03:16:18 +0900

    edit header

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -6,8 +6,6 @@ export default function Header({ className, ...props }) {
       className={`z-50 w-full p-4 h-16 bg-white/40 backdrop-blur-xl flex -m-b-16 flex-row items-center justify-between ${className}`}
     >
       <Link href={"/"}>npuis</Link>
-      <Link href={"/signout"}>signout</Link>
-      <Link href={"/signin"}>signIn</Link>
     </header>
   );
 }
