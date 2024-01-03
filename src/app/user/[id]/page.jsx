ce292f98a473794ee4da37910c37898176775d6b
pr-citrate <pr.citrate@gmail.com>
2024-01-03 17:56:03 +0900

    fixed api call

--- src/app/user/[id]/page.jsx
+++ src/app/user/[id]/page.jsx
@@ -4,6 +4,13 @@ export default function UserPage({ params }) {
   const id = params.id;
 
   return (
-    <Image src={`/api/images/user/${id}`} alt="user" width={100} height={100} />
+    <Image
+      src={`${
+        process.env.NODE_ENV === "production" ? "https://npius.dev" : ""
+      }/api/images/user/${id}`}
+      alt="user"
+      width={100}
+      height={100}
+    />
   );
 }
